b7f2500b33dd8078686798004a87e0626192372c
Elfling <tomasz@kominiarczuk.com>
2025-08-02 10:44:02 +0200

    Changed colliders to new assets

--- Scripts/colliders.gd
+++ Scripts/colliders.gd
@@ -3,8 +3,9 @@ class_name Colliders
 
 static func items():
 	return [
-		preload("res://GameObjects/Colliders/box.tscn"),
+		preload("res://GameObjects/Colliders/barrier.tscn"),
 		preload("res://GameObjects/Colliders/trash_can.tscn"),
+		preload("res://GameObjects/Colliders/car.tscn"),
 	]
 
 
